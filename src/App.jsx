48e20e508688e74d76348c078c059b0b562f311b
Ala El Achkar <iamalaaashkar97@gmail.com>
2023-10-25 23:09:04 +0300

    fixed itemLayout navigation restriction

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.scss'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { CustomerService } from "./views/CustomerService/CustomerService";
 import { News } from "./views/News/News";
 import { Store } from "./views/Store/Store";
@@ -12,25 +12,30 @@ import { MainLayout } from "./Layouts/MainLayout/MainLayout"
 import { Kids } from "./views/Kids/Kids";
 import { Baby } from "./views/Baby/Baby";
 import { ProductPage } from "./views/ProductPage/ProductPage";
+import { ErrorPage } from "./views/ErrorPage/ErrorPage";
 
 const App = () => {
+  const shouldRedirectToErrorPage = window.location.pathname === '/items' || window.location.pathname === '/items/'; // Redirect if the path is "/items"
+
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />} >
+      <Route path="/" element={<MainLayout />}>
         <Route index element={<Home />} />
         <Route path="customer-service" element={<CustomerService />} />
         <Route path="fashion-news" element={<News />} />
         <Route path="store" element={<Store />} />
         <Route path="productPage" element={<ProductPage />} />
-        <Route path="items" element={<ItemsLayout />}>
+        <Route path="items" element={shouldRedirectToErrorPage ? <Navigate to="/not-found" /> : <ItemsLayout />}>
           <Route path="woman" element={<Woman />} />
           <Route path="man" element={<Male />} />
-          <Route path="kids" element={< Kids />} />
-          <Route path="baby" element={< Baby />} />
+          <Route path="kids" element={<Kids />} />
+          <Route path="baby" element={<Baby />} />
         </Route>
       </Route>
-    </Routes >
-  )
-};
+      <Route path="not-found" element={<ErrorPage />} />
+      <Route path="*" element={<Navigate to="/not-found" />} />
+    </Routes>
+  );
+}
 
 export default App;
--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -2,19 +2,25 @@ import './BreadCrumb.scss';
 import { useLocation, Link } from 'react-router-dom';
 
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export default function Breadcrumb() {
   const location = useLocation();
   const pathSegments = location.pathname.split('/').filter(segment => segment);
+
+  const filteredSegments = pathSegments.filter(segment => segment !== 'items');
 
   console.log(pathSegments);
 
   return (
     <div className="breadcrumb">
       <ul>
-        <li><Link to="/">Home</Link></li>
-        {pathSegments.map((segment, index) => (
+        <li><Link to="/">START PAGE</Link></li>
+        {filteredSegments.map((segment, index) => (
           <li key={index}>
-            <Link to={`/${pathSegments.slice(0, index + 1).join('/')}`}>{`/${segment}`}</Link>
+            <Link to={`/items/${filteredSegments.slice(0, index + 1).join('/')}`}>{`/${capitalizeFirstLetter(segment)}`}</Link>
           </li>
         ))}
       </ul>
